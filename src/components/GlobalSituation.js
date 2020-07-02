e4a37121c43d2a17577c04ba7b94248bd73dbf8c
amasudi <a.masudi@gmail.com>
2020-07-02 11:20:17 +0500

    removed errors from context

--- src/components/GlobalSituation.js
+++ src/components/GlobalSituation.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Grid, FormControl, NativeSelect } from "@material-ui/core";
+import React, { useEffect, useContext } from "react";
+import { Grid, Typography, FormControl, NativeSelect } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { RegionalSummary } from "./RegionalSummary";
 import { GlobalSummary } from "./GlobalSummary";
@@ -18,9 +18,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 export const GlobalSituation = () => {
-  const { country, handleActions } = useContext(GlobalContext);
+  function isEmpty(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+  }
+  const { data, handleActions } = useContext(GlobalContext);
   const classes = useStyles();
-  const [regionalData, setRegionalData] = useState([]);
+  //const [regionalData, setRegionalData] = useState([]);
   const handleChange = (event) => {
     handleChangeCountry(event.target.value);
   };
@@ -32,51 +38,59 @@ export const GlobalSituation = () => {
       let response = await fetch("https://disease.sh/v2/continents");
       let data = await response.json();
       handleActions("SET_REGIONAL", { regionalData: data });
-      setRegionalData(data);
+      //setRegionalData(data);
     };
     fetchData();
-  }, []);
-  return (
-    <div style={{ margin: "25px" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <img
-            src={coronaLogo}
-            alt="COVID-19"
-            style={{
-              marginLeft: "auto",
-              marginRight: "auto",
-              display: "flex",
-              maxWidth: "90%",
-            }}
-          />
+  }, [handleActions]);
+  if (isEmpty(data.regionalData)) {
+    return <Typography>...Loading</Typography>;
+  } else {
+    return (
+      <div style={{ margin: "25px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <img
+              src={coronaLogo}
+              alt="COVID-19"
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "flex",
+                maxWidth: "90%",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl xs={12} className={classes.formControl}>
+              <NativeSelect
+                xs={12}
+                value={data.country}
+                onChange={handleChange}
+              >
+                <option value="">---Select Country---</option>
+                {data.regionalData.map((row, ind) => (
+                  <optgroup label={row.continent} key={ind}>
+                    {row.countries.map((countryVal, i) => (
+                      <option value={countryVal} key={i}>
+                        {countryVal}
+                      </option>
+                    ))}
+                  </optgroup>
+                ))}
+              </NativeSelect>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <GlobalSummary />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <RegionalSummary />
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <CountryWiseDetails />
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <FormControl xs={12} className={classes.formControl}>
-            <NativeSelect xs={12} value={country} onChange={handleChange}>
-              <option value="">---Select Country---</option>
-              {regionalData.map((row, ind) => (
-                <optgroup label={row.continent} key={ind}>
-                  {row.countries.map((countryVal, i) => (
-                    <option value={countryVal} key={i}>
-                      {countryVal}
-                    </option>
-                  ))}
-                </optgroup>
-              ))}
-            </NativeSelect>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <GlobalSummary />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <RegionalSummary />
-        </Grid>
-        <Grid item xs={12} sm={12}>
-          <CountryWiseDetails />
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 };
